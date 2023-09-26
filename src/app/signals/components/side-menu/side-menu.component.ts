bd4f090b7282ad213f34ddee5c5d0b636f754b5c
Matías Roy Rodriguez <matiasrr980@gmail.com>
2023-09-25 21:24:09 -0300

    feat: change menu to signal type

--- src/app/signals/components/side-menu/side-menu.component.ts
+++ src/app/signals/components/side-menu/side-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 interface MenuItem {
   title: string;
@@ -13,10 +13,16 @@ interface MenuItem {
 export class SideMenuComponent {
 
 
-  public menuItems: MenuItem[] = [
+  public menuItems = signal<MenuItem[]>([
     { title: 'Contador', route: 'counter' },
     { title: 'Usuario', route: 'user-info' },
-    { title: 'Mutaciones', route: 'properties' },
-  ]
+    { title: 'Mutaciones', route: 'properties' }
+  ]);
+
+  // public menuItems: MenuItem[] = [
+  //   { title: 'Contador', route: 'counter' },
+  //   { title: 'Usuario', route: 'user-info' },
+  //   { title: 'Mutaciones', route: 'properties' },
+  // ]
 
 }
